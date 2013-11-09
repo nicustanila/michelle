0ff56bb7ebc899bf9eaaf6e5827bea544d9fc442
nicu <nicu.stanila@brandup.ro>
2013-11-09 15:06:35 +0200

    test

--- c2runtime.js
+++ c2runtime.js
@@ -11093,6 +11093,259 @@ cr.do_cmp = function (x, cmp, y)
 cr.shaders = {};
 ;
 ;
+cr.plugins_.Rex_Video = function(runtime)
+{
+	this.runtime = runtime;
+};
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Video.prototype;
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	var typeProto = pluginProto.Type.prototype;
+	typeProto.onCreate = function()
+	{
+	};
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	var instanceProto = pluginProto.Instance.prototype;
+	instanceProto.onCreate = function()
+	{
+		if (this.runtime.isDomFree)
+			return;
+        this.elem = document.createElement("video");
+        var source = document.createElement('source');
+        this.elem.appendChild(source);
+        source.src = this.properties[0];
+        source = document.createElement('source');
+        this.elem.appendChild(source);
+        source.src = this.properties[1];
+        source = document.createElement('source');
+        this.elem.appendChild(source);
+        source.src = this.properties[2];
+        this.elem.poster = this.properties[3];
+        this.elem.autoplay = (this.properties[4]==1);
+        this.elem.controls = (this.properties[5]==1);
+        this.elem.preload = ["auto","metadata","none"][this.properties[6]];
+        this.elem.loop = (this.properties[7]==1);
+        this.elem.muted = (this.properties[8]==1);
+        this.elem.id = this.properties[9];
+        jQuery(this.elem).appendTo(this.runtime.canvasdiv ? this.runtime.canvasdiv : "body");
+        this._pre_ended = false;
+        this._checked_is_playing = false;
+		this.updatePosition();
+		this.runtime.tickMe(this);
+	};
+	instanceProto.onDestroy = function ()
+	{
+		if (this.runtime.isDomFree)
+			return;
+		jQuery(this.elem).remove();
+		this.elem = null;
+	};
+	instanceProto.tick = function ()
+	{
+		this.updatePosition();
+        this.check_ended();
+        this.check_playing();
+	};
+	instanceProto.updatePosition = function ()
+	{
+		var left = this.layer.layerToCanvas(this.x, this.y, true);
+		var top = this.layer.layerToCanvas(this.x, this.y, false);
+		var right = this.layer.layerToCanvas(this.x + this.width, this.y + this.height, true);
+		var bottom = this.layer.layerToCanvas(this.x + this.width, this.y + this.height, false);
+		if (!this.visible || right <= 0 || bottom <= 0 || left >= this.runtime.width || top >= this.runtime.height)
+		{
+			jQuery(this.elem).hide();
+			return;
+		}
+		if (left < 1)
+			left = 1;
+		if (top < 1)
+			top = 1;
+		if (right >= this.runtime.width)
+			right = this.runtime.width - 1;
+		if (bottom >= this.runtime.height)
+			bottom = this.runtime.height - 1;
+		jQuery(this.elem).show();
+		var offx = left + jQuery(this.runtime.canvas).offset().left;
+		var offy = top + jQuery(this.runtime.canvas).offset().top;
+		jQuery(this.elem).offset({left: offx, top: offy});
+		jQuery(this.elem).width(right - left);
+		jQuery(this.elem).height(bottom - top);
+	};
+	instanceProto.draw = function(ctx)
+	{
+	};
+	instanceProto.drawGL = function(glw)
+	{
+	};
+	instanceProto.check_ended = function ()
+	{
+        if (!this._pre_ended && this.elem.ended)
+        {
+            this._checked_is_playing = false;
+            this.runtime.trigger(cr.plugins_.Rex_Video.prototype.cnds.OnEnded, this);
+		}
+        this._pre_ended = this.elem.ended;
+	};
+	instanceProto.check_playing = function()
+	{
+		if((!this._checked_is_playing) &&
+		   (this.elem.currentTime != this.elem.initialTime))
+		{
+			this._checked_is_playing = true;
+			this.runtime.trigger(cr.plugins_.Rex_Video.prototype.cnds.OnPlay, this);
+		}
+	};
+	instanceProto.IsPlaying = function()
+	{
+		return ((!this.elem.paused) && (!this.elem.ended));
+	};
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();
+	Cnds.prototype.OnEnded = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.IsEnded = function ()
+	{
+		return this.elem.ended;
+	};
+	Cnds.prototype.OnPlay = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.IsPlaying = function ()
+	{
+		return this.IsPlaying();
+	};
+	Cnds.prototype.CompareCurrentTime = function (cmp, d)
+	{
+	    if (this.IsPlaying())
+		    return cr.do_cmp(this.elem.currentTime, cmp, d);
+		else
+		    return false;
+	};
+	function Acts() {};
+	pluginProto.acts = new Acts();
+	Acts.prototype.SetSource = function (src)
+	{
+		this.elem.src = src;
+	};
+	Acts.prototype.Play = function ()
+	{
+		this.elem.play();
+	};
+	Acts.prototype.Stop = function ()
+	{
+		this.elem.pause();
+		this.elem.initialTime = 0;
+		this.elem.currentTime = 0;
+		this._checked_is_playing = false;
+	};
+	Acts.prototype.Pause = function ()
+	{
+		this._checked_is_playing = false;
+		this.elem.pause();
+	};
+	Acts.prototype.SetControls = function (is_enable)
+	{
+		this.elem.controls = (is_enable==1);
+	};
+	Acts.prototype.SetVolume = function (volume)
+	{
+		this.elem.volume = cr.clamp(volume, 0, 1);
+	};
+	Acts.prototype.SetPoster = function (poster)
+	{
+		this.elem.poster = poster;
+	};
+	Acts.prototype.SetLoop = function (is_enable)
+	{
+		this.elem.loop = (is_enable==1);
+	};
+	Acts.prototype.SetMuted = function (is_enable)
+	{
+		this.elem.muted = (is_enable==1);
+	};
+	Acts.prototype.SetAutoplay = function (is_enable)
+	{
+		this.elem.autoplay = (is_enable==1);
+	};
+	Acts.prototype.SetVisible = function (vis)
+	{
+		this.visible = (vis !== 0);
+	};
+	Acts.prototype.Seek = function (t)
+	{
+		this.elem.currentTime = t;
+	};
+	Acts.prototype.Image2Canvas = function (objType)
+	{
+	    if (objType == null)
+	        return;
+		var sol = objType.getCurrentSol();
+		var instances;
+		if (sol.select_all)
+			instances = sol.type.instances;
+		else
+			instances = sol.instances;
+        var i, cnt=instances.length, canvas_obj, ctx;
+        debugger;
+        for (i=0; i<cnt; i++)
+        {
+            canvas_obj = instances[i];
+            ctx = canvas_obj.ctx;
+            ctx.save();
+			ctx.scale(canvas_obj.canvas.width/this.elem.videoWidth, canvas_obj.canvas.height/this.elem.videoHeight);
+			ctx.rotate(-canvas_obj.angle);
+			ctx.translate(-canvas_obj.bquad.tlx, -canvas_obj.bquad.tly);
+			ctx.drawImage(this.elem, 0, 0);
+			ctx.restore();
+        }
+        this.runtime.redraw = true;
+	};
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	Exps.prototype.CurrentTime = function (ret)
+	{
+		ret.set_float(this.elem.currentTime);
+	};
+	Exps.prototype.IsPaused = function (ret)
+	{
+		ret.set_int(this.elem.paused);
+	};
+	Exps.prototype.IsMuted = function (ret)
+	{
+		ret.set_int(this.elem.muted);
+	};
+	Exps.prototype.Volume = function (ret)
+	{
+		ret.set_float(this.elem.volume);
+	};
+	Exps.prototype.ReadyState = function (ret)
+	{
+		ret.set_int(this.elem.readyState);
+	};
+	Exps.prototype.SourceHeight = function (ret)
+	{
+		ret.set_float(this.elem.videoHeight);
+	};
+	Exps.prototype.SourceWidth = function (ret)
+	{
+		ret.set_float(this.elem.videoWidth);
+	};
+}());
+;
+;
 cr.plugins_.Sprite = function(runtime)
 {
 	this.runtime = runtime;
@@ -14493,6 +14746,18 @@ cr.getProjectModel = function() { return [
 	null,
 	[
 	[
+		cr.plugins_.Rex_Video,
+		false,
+		true,
+		true,
+		true,
+		false,
+		false,
+		false,
+		false,
+		false
+	]
+,	[
 		cr.plugins_.Sprite,
 		false,
 		true,
@@ -14718,7 +14983,7 @@ cr.getProjectModel = function() { return [
 			false,
 			9837469744955384,
 			[
-				["images/sprite-sheet0.png", 70368, 0, 0, 467, 302, 1, 0.501071, 0.5,[],[-0.376874,-0.307947,-0.00214136,-0.42053,0.374732,-0.307947,0.498929,0,0.284797,0.168874,-0.00214136,0.496689,-0.286938,0.168874,-0.501071,0],0]
+				["images/sprite-sheet0.png", 70763, 0, 0, 467, 302, 1, 0.501071, 0.5,[],[-0.376874,-0.307947,-0.00214168,-0.42053,0.374732,-0.307947,0.498929,0,0.284796,0.168874,-0.00214168,0.496689,-0.286938,0.168874,-0.501071,0],0]
 			]
 			]
 		],
@@ -14751,6 +15016,22 @@ cr.getProjectModel = function() { return [
 		[]
 		,[1]
 	]
+,	[
+		"t10",
+		cr.plugins_.Rex_Video,
+		false,
+		[],
+		0,
+		0,
+		null,
+		null,
+		[
+		],
+		false,
+		false,
+		4073775889025031,
+		[]
+	]
 	],
 	[
 	],
@@ -14765,9 +15046,9 @@ cr.getProjectModel = function() { return [
 		[
 		[
 			"start1",
-			1,
+			0,
 			1950338125178332,
-			true,
+			false,
 			[102, 204, 204],
 			true,
 			1,
@@ -14945,6 +15226,45 @@ cr.getProjectModel = function() { return [
 					"Default",
 					0,
 					1
+				]
+			]
+			],
+			[			]
+		]
+,		[
+			"video",
+			1,
+			1743424469555384,
+			true,
+			[255, 255, 255],
+			true,
+			1,
+			1,
+			1,
+			false,
+			1,
+			0,
+			0,
+			[
+			[
+				[0, 0, 0, 1920, 1080, 0, 0, 1, 0, 0, 0, 0, []],
+				10,
+				10,
+				[
+				],
+				[
+				],
+				[
+					"Files/colin-abroad.m4v",
+					"",
+					"",
+					"",
+					1,
+					1,
+					0,
+					0,
+					0,
+					""
 				]
 			]
 			],
@@ -15529,7 +15849,7 @@ cr.getProjectModel = function() { return [
 					0,
 					[
 						0,
-						957
+						958
 					]
 				]
 				]
@@ -15615,7 +15935,7 @@ cr.getProjectModel = function() { return [
 						7,
 						[
 							0,
-							900
+							910
 						]
 					]
 					]
@@ -15630,6 +15950,35 @@ cr.getProjectModel = function() { return [
 					false
 				]
 				]
+			]
+			]
+		]
+,		[
+			0,
+			null,
+			false,
+			null,
+			9527414963548139,
+			[
+			[
+				-1,
+				cr.system_object.prototype.cnds.OnLayoutStart,
+				null,
+				1,
+				false,
+				false,
+				false,
+				2742075779932734,
+				false
+			]
+			],
+			[
+			[
+				10,
+				cr.plugins_.Rex_Video.prototype.acts.Play,
+				null,
+				4196300627300626,
+				false
 			]
 			]
 		]
